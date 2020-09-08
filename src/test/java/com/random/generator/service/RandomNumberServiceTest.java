95cf3fcf37f212cbc94ed900a00d8f40350bd0da
emilzajac <emilzajac@gmail.com>
2020-09-08 09:18:21 +0200

    update test

--- src/test/java/com/random/generator/service/RandomNumberServiceTest.java
+++ src/test/java/com/random/generator/service/RandomNumberServiceTest.java
@@ -2,19 +2,19 @@ package com.random.generator.service;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = "liquibase.change-log=classpath:db.changelog/db.changelog-master-test.xml")
+@TestPropertySource(properties = "resources/application-test.properties")
 class RandomNumberServiceTest {
 
     @Autowired
@@ -26,6 +26,6 @@ class RandomNumberServiceTest {
         List<Integer> result = randomNumberService.findAll();
 
         // then
-        Assertions.assertThat(result).hasSize(13);
+        Assertions.assertThat(result).hasSize(12);
     }
 }
